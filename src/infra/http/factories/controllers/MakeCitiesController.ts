6cf284f12b2bcc3638865b0f3facba2f288a2f02
Maxmiller Nunes <maxmillernunes11@gmail.com>
2021-10-07 17:23:10 -0300

    feat(http): add new controller and router for list cities

--- src/infra/http/factories/controllers/MakeCitiesController.ts
+++ src/infra/http/factories/controllers/MakeCitiesController.ts
@@ -1,5 +1,7 @@
-import { CreateCityController } from '@modules/city/useCases';
+import { CreateCityController } from '@modules/city/useCases/CreateCity';
+import { ListCitiesController } from '@modules/city/useCases/ListCity';
 
 const createCityController = new CreateCityController();
+const listCitiesController = new ListCitiesController();
 
-export { createCityController };
+export { createCityController, listCitiesController };
--- src/infra/http/routes/city.routes.ts
+++ src/infra/http/routes/city.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
-import { createCityController } from '../factories/controllers/MakeCitiesController';
+import {
+  createCityController,
+  listCitiesController,
+} from '../factories/controllers/MakeCitiesController';
 
 const citiesRoutes = Router();
 
 citiesRoutes.post('/', createCityController.handle);
+citiesRoutes.get('/', listCitiesController.handle);
 
 export { citiesRoutes };
