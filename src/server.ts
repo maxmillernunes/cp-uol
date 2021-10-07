7bb084bbfae5d0a49d5453796f6d438f4f7573a9
Maxmiller Nunes <maxmillernunes11@gmail.com>
2021-10-07 02:29:20 -0300

    refactor: move app and server for infra

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-
-import { app } from './app';
-
-const { API_PORT } = process.env;
-
-app.listen(API_PORT, () => {
-  console.log(`Servidor rodando! PORTA INTERNA:${API_PORT}`);
-});
