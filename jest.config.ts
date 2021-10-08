5244b30d3be4bc26ef140324a482853686be5075
Maxmiller Nunes <maxmillernunes11@gmail.com>
2021-10-08 01:15:40 -0300

    feat(jest): add jest a project

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { pathsToModuleNameMapper } from 'ts-jest/utils';
+
+import { compilerOptions } from './tsconfig.json';
+
+export default {
+  bail: true,
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageReporters: ['text-summary', 'lcov'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/src/',
+  }),
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testMatch: ['**/*.spec.ts'],
+  watchPathIgnorePatterns: ['node_modules'],
+  transformIgnorePatterns: ['node_modules'],
+  collectCoverageFrom: ['src/modules/**/useCases/**/*.ts'],
+};
