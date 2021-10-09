4bd716e389317f9229c916e0bacc897aa26f2b9b
Maxmiller Nunes <maxmillernunes11@gmail.com>
2021-10-09 11:53:04 -0300

    feat(http): add controllers remove and update customer

--- src/infra/http/factories/controllers/MakeCustomersController.ts
+++ src/infra/http/factories/controllers/MakeCustomersController.ts
@@ -1,13 +1,19 @@
 import { CreateCustomerController } from '@modules/customers/useCases/CreateCustomer';
 import { FindCustomerController } from '@modules/customers/useCases/FindCustomer';
 import { ListCustomersController } from '@modules/customers/useCases/ListCustomers';
+import { RemoveCustomerController } from '@modules/customers/useCases/RemoveCustomer';
+import { UpdateCustomerController } from '@modules/customers/useCases/UdateCustomer';
 
 const createCustomerController = new CreateCustomerController();
 const listCustomersController = new ListCustomersController();
 const findCustomerController = new FindCustomerController();
+const removeCustomerController = new RemoveCustomerController();
+const updateCustomerController = new UpdateCustomerController();
 
 export {
   createCustomerController,
   listCustomersController,
   findCustomerController,
+  removeCustomerController,
+  updateCustomerController,
 };
--- src/infra/http/routes/customers.routes.ts
+++ src/infra/http/routes/customers.routes.ts
@@ -4,6 +4,8 @@ import {
   createCustomerController,
   findCustomerController,
   listCustomersController,
+  removeCustomerController,
+  updateCustomerController,
 } from '../factories/controllers/MakeCustomersController';
 
 const customersRoutes = Router();
@@ -11,5 +13,8 @@ const customersRoutes = Router();
 customersRoutes.post('/', createCustomerController.handle);
 customersRoutes.get('/', listCustomersController.handle);
 customersRoutes.get('/:customer_id', findCustomerController.handle);
+customersRoutes.post('/:customer_id', removeCustomerController.handle);
+customersRoutes.delete('/:customer_id', removeCustomerController.handle);
+customersRoutes.patch('/:customer_id', updateCustomerController.handle);
 
 export { customersRoutes };
